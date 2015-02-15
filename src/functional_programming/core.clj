6527696b08b138ccb0188db5fd5fb87c4366a4ee
Patrick Martin <prof.pmartin10@gmail.com>
2015-02-14 20:38:25 -0500

    Basic functional implementations of summing a list of integers.

--- src/functional_programming/core.clj
+++ src/functional_programming/core.clj
@@ -1,6 +1,13 @@
 (ns functional-programming.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; Performing accumulation with recursion
+(defn sum [nums]
+  (loop [acc 0
+         curr-nums nums]
+    (if (empty? curr-nums)
+      acc
+      (recur (+ acc (first curr-nums)) (rest curr-nums)))))
+
+; and better with reduce!
+(defn reduce-sum [nums]
+  (reduce (fn [n1 n2] (+ n1 n2)) 0 nums))
