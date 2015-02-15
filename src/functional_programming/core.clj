c32b1b8d9a624fda041419adec46ece93c6a68f4
Patrick Martin <prof.pmartin10@gmail.com>
2015-02-14 21:20:45 -0500

    Made a parallel version of sum using fold - never used that function before.

--- src/functional_programming/core.clj
+++ src/functional_programming/core.clj
@@ -1,4 +1,5 @@
-(ns functional-programming.core)
+(ns functional-programming.core
+  (:require [clojure.core.reducers :as r]))
 
 ; Performing accumulation with recursion
 (defn sum [nums]
@@ -11,3 +12,7 @@
 ; and better with reduce!
 (defn reduce-sum [nums]
   (reduce (fn [n1 n2] (+ n1 n2)) 0 nums))
+
+; using fold to parallelize sum
+(defn parallel-sum [nums]
+  (r/fold + nums))
