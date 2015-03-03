4c8db4da46f5e38eeb8de1e64097c85e1654f84a
Patrick Martin <prof.pmartin10@gmail.com>
2015-03-03 08:06:44 -0500

    Finished off day 2 in the functional programming chapter.

--- src/functional_programming/day2.clj
+++ src/functional_programming/day2.clj
@@ -1,26 +1,7 @@
 (ns functional-programming.day2
-  (:require [functional-programming.day1 :as day1]
-            [clojure.core.reducers :as r]
+  (:require [clojure.core.reducers :as r]
             [clojure.core.protocols :refer [CollReduce coll-reduce]]
             [clojure.core.reducers :refer [CollFold coll-fold]]))
-
-; This code follows Day 2 within Chapter 3.
-; To start, utilize the function merge-with to combine parallel word count operations.
-(defn count-words-parallel [pages]
-  (reduce (partial merge-with +)
-          (pmap (fn [p] (frequencies (day1/get-words p))) pages)))
-
-; This implementation is still not as fast as it could be. The pages could be batched with partition!
-(defn count-words-batched [pages]
-  (reduce (partial merge-with +)
-          (pmap (fn [p] (frequencies (day1/get-words p))) (partition-all 100 pages))))
-
-; Messing with reducers...
-; A reducer returns a recipe to generate a result. Reducers are NOT executed until they
-; are passed to fold or reduce.
-(def mult-2-map
-  (r/map (partial * 2) (range 1 10))) ; r/map is a recipe to generate a sequence of square numbers
-(reduce conj [] mult-2-map)
 
 ; Making our own reducer
 (defn my-reduce
@@ -40,9 +21,10 @@
       (coll-reduce reducible (transform-fn f1) init))))
 
 (defn my-map [map-fn reducible]
+  "Returns a reducer that performs map."
   (make-reducer reducible
                 (fn [reduce-fn]
-                  (println reduce-fn) ; just to see wha the reduce-fn
+;                  (println reduce-fn) ; just to see what the reduce-fn is...
                   (fn [acc val]
                     (reduce-fn acc (map-fn val))))))
 
@@ -52,5 +34,5 @@
   ([combine-fn reduce-fn coll]
     (my-fold 512 combine-fn reduce-fn coll))
   ([n combine-fn reduce-fn coll]
-    (println "combine: " combine-fn " reduce: " reduce-fn " coll: " coll)
+    (println "combine: " combine-fn " reduce: " reduce-fn)
     (coll-fold coll n combine-fn reduce-fn)))
---
+++ test/functional_programming/day2_test.clj
@@ -0,0 +1,36 @@
+(ns functional-programming.day2-test
+  (:require [functional-programming.day2 :refer :all]
+            [functional-programming.day1 :as day1]
+            [clojure.core.reducers :as r]))
+
+; This code follows Day 2 within Chapter 3.
+; To start, utilize the function merge-with to combine parallel word count operations.
+(defn count-words-parallel [pages]
+  (reduce (partial merge-with +)
+          (pmap (fn [p] (frequencies (day1/get-words p))) pages)))
+
+; This implementation is still not as fast as it could be. The pages could be batched with partition!
+(defn count-words-batched [pages]
+  (reduce (partial merge-with +)
+          (pmap (fn [p] (frequencies (day1/get-words p))) (partition-all 100 pages))))
+
+; Messing with reducers...
+; A reducer returns a recipe to generate a result. Reducers are NOT executed until they
+; are passed to fold or reduce.
+(def mult-2-map
+  (r/map (partial * 2) (range 1 10))) ; r/map is a recipe to generate a sequence of square numbers
+(reduce conj [] mult-2-map)
+
+; Testing my-fold and my-map to perform a fold-reduce
+(let [v (into [] (range 1000))]
+  (my-fold + (my-map (partial * 3) v)))
+
+(defn parallel-frequencies [coll]
+  (my-fold
+    (partial merge-with +)
+    (fn counter-fn [counts k]
+      (assoc counts k (inc (get counts k 0))))
+    coll))
+
+(let [nums (into [] (take 10000000 (repeatedly (fn rand20-fn [] (rand-int 20)))))] ; lots of random ints!
+  (parallel-frequencies nums))
